36d912bbe5d978827ad46a4cd4a6d24bed5b5823
Jon Layton <me@jonl.io>
2018-07-21 17:27:01 -0500

    [consensus] Finish consensus/joinsplit.cpp & h

--- src/consensus/joinsplit.cpp
+++ src/consensus/joinsplit.cpp
@@ -1,18 +1,33 @@
-bool CheckTransactionJoinsplits(const CTransaction& tx, CValidationState &state)
+
+// Copyright (c) 2018 The Bitcoin Private Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <consensus/joinsplit.h>
+
+#include <init.h>
+#include <primitives/transaction.h>
+#include <consensus/validation.h>
+#include <script/interpreter.h>
+#include <script/script.h>
+#include <zcash/Zcash.h>
+#include <zcash/Proof.hpp>
+
+bool CheckTransactionJoinSplits(const CTransaction& tx, CValidationState &state, unsigned int flags)
 {
     if (tx.vjoinsplit.size() > 0) {
         // Empty output script.
         CScript scriptCode;
         uint256 dataToBeSigned;
         int hashtype = SIGHASH_ALL;
-        if(flags & SCRIPT_VERIFY_FORKID)
+        if (flags & SCRIPT_VERIFY_FORKID)
             hashtype |= SIGHASH_FORKID;
 
         try {
             dataToBeSigned = SignatureHash(scriptCode, tx, NOT_AN_INPUT,
                                            hashtype, (flags & SCRIPT_VERIFY_FORKID) ? FORKID_IN_USE : FORKID_NONE);
         } catch (std::logic_error ex) {
-            return state.DoS(100, error("CheckTransaction(): error computing signature hash"),
+            return state.DoS(100, error("CheckTransactionJoinSplits(): error computing signature hash"),
                              REJECT_INVALID, "error-computing-signature-hash");
         }
 
@@ -25,15 +40,15 @@ bool CheckTransactionJoinsplits(const CTransaction& tx, CValidationState &state)
                                         tx.joinSplitPubKey.begin()
                 ) != 0) {
 
-            return state.DoS(100, error("CheckTransaction(): invalid joinsplit signature"),
+            return state.DoS(100, error("CheckTransactionJoinSplits(): invalid joinsplit signature"),
                              REJECT_INVALID, "bad-txns-invalid-joinsplit-signature");
         }
 
         // Ensure that zk-SNARKs verify
         auto verifier = libzcash::ProofVerifier::Strict();
-        for(const JSDescription &joinsplit : tx.vjoinsplit) {
+        for (const JSDescription& joinsplit : tx.vjoinsplit) {
             if (!joinsplit.Verify(pzcashParams.get(), verifier, tx.joinSplitPubKey)) {
-                return state.DoS(100, error("CheckTransaction(): joinsplit does not verify"),
+                return state.DoS(100, error("CheckTransactionJoinSplits(): joinsplit does not verify"),
                                  REJECT_INVALID, "bad-txns-joinsplit-verification-failed");
             }
         }
--- src/consensus/joinsplit.h
+++ src/consensus/joinsplit.h
@@ -1,10 +1,13 @@
-// Copyright (c) 2018      The Bitcoin Private developers
+// Copyright (c) 2018 The Bitcoin Private Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BTCP_CONSENSUS_JOINSPLIT_H
 #define BTCP_CONSENSUS_JOINSPLIT_H
 
-bool CheckTransactionJoinsplits(const CTransaction& tx, CValidationState &state);
+#include <primitives/transaction.h>
+#include <consensus/validation.h>
+
+bool CheckTransactionJoinSplits(const CTransaction& tx, CValidationState& state, unsigned int flags);
 
 #endif
