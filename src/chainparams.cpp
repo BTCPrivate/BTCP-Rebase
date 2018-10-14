d830d321afc50a0f066a41287be8ee56f918f811
J62 <j.owens@interbiznw.com>
2018-10-14 00:45:31 -0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -261,6 +261,7 @@ public:
         assert(consensus.hashGenesisBlock == uint256S("0x03e1c4bb705c871bf9bfda3e74b7f8f86bff267993c215a89d5795e3708e5e1f"));
 
         vSeeds.emplace_back("rebase-testnet.btcprivate.co");
+        vSeeds.emplace_back("35.233.205.224");
 
         // guarantees the first 2 characters, when base58 encoded, are "n1"
         base58Prefixes[PUBKEY_ADDRESS]     = {0x19,0x57};
