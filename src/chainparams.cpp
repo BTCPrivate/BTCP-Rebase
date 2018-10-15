4e10a6272e8a087e006db941a17a3d1563115a3b
Jon Layton <me@jonl.io>
2018-10-14 22:05:38 -0500

    Activate BIP34 at block 17 for test networks, the smallest value possibly for a CScriptNum to be represented as expected

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -215,7 +215,7 @@ public:
         consensus.nSubsidyHalvingInterval = 840000;
 
         // P2SH, height in coinbase, CLTV and DERSIG all enforced from genesis
-        consensus.BIP34Height = 0;
+        consensus.BIP34Height = 17;
         consensus.BIP65Height = 0;
         consensus.BIP66Height = 0;
 
@@ -320,7 +320,7 @@ public:
         consensus.nSubsidyHalvingInterval = 840000;
 
         // P2SH, height in coinbase, CLTV and DERSIG all enforced from genesis
-        consensus.BIP34Height = 0;
+        consensus.BIP34Height = 17;
         consensus.BIP65Height = 0;
         consensus.BIP66Height = 0;
 
